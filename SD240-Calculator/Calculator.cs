e6dce541fc5a859c0268b7fd657aa4affdf8c9b5
Edgardo Jose Bulaong <ejbulaong@yahoo.com>
2019-05-01 08:30:16 -0500

    First Commit

---
+++ SD240-Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD240_Calculator
+{
+    public class Calculator
+    {
+        public double EnteredValue { get; set; }
+        public double Answer { get; set; }
+        public string Operator { get; set; }
+    }
+}
