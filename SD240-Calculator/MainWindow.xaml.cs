d4acbe1ccaffc2b4dc11aa642c70e8be2233e841
Edgardo Jose Bulaong <ejbulaong@yahoo.com>
2019-05-02 08:49:10 -0500

    decimal only showing once

--- SD240-Calculator/MainWindow.xaml.cs
+++ SD240-Calculator/MainWindow.xaml.cs
@@ -94,28 +94,39 @@ namespace SD240_Calculator
             txtDisplay.Text = Calculator.EnteredValue;
         }
 
+        private void BtnDecimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Calculator.EnteredValue.Contains("."))
+                if (Calculator.EnteredValue == string.Empty)
+                {
+                    Calculator.EnteredValue += "0.";
+                    txtDisplay.Text = Calculator.EnteredValue;
+                }
+                else
+                {
+                    Calculator.EnteredValue += ".";
+                    txtDisplay.Text = Calculator.EnteredValue;
+                }
+        }
+
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            Calculator.Operator = "+";
-            Evaluate(Calculator.Operator);
+            CallBasicCalculation("+");
         }
 
         private void BtnSubtract_Click(object sender, RoutedEventArgs e)
         {
-            Calculator.Operator = "-";
-            Evaluate(Calculator.Operator);
+            CallBasicCalculation("-");
         }
 
         private void BtnMultiply_Click(object sender, RoutedEventArgs e)
         {
-            Calculator.Operator = "*";
-            Evaluate(Calculator.Operator);
+            CallBasicCalculation("*");
         }
 
         private void BtnDivide_Click(object sender, RoutedEventArgs e)
         {
-            Calculator.Operator = "/";
-            Evaluate(Calculator.Operator);
+            CallBasicCalculation("/");
         }
 
         private void BtnEquals_Click(object sender, RoutedEventArgs e)
@@ -134,27 +145,72 @@ namespace SD240_Calculator
             lblOperator.Content = Calculator.Operator;
         }
 
-        private void Evaluate(string mathOperator)
+        private void CallBasicCalculation(string mathOperator)
         {
-            if (Calculator.StoredValue == "")
+            if (Calculator.Operator != mathOperator && Calculator.Operator != "")
             {
-                Calculator.StoredValue = Calculator.EnteredValue;
+                Evaluate(Calculator.Operator);
+                Calculator.Operator = mathOperator;
+                lblOperator.Content = Calculator.Operator;
             }
-            else if (Calculator.StoredValue != "" && Calculator.EnteredValue != "")
+            else
             {
-                var expression = Calculator.StoredValue + Calculator.Operator + Calculator.EnteredValue;
-                double answer;
-                answer = Convert.ToDouble(new DataTable().Compute(expression, null));
-                Calculator.StoredValue = answer.ToString();
+                Calculator.Operator = mathOperator;
+                Evaluate(Calculator.Operator);
             }
-
-            Calculator.EnteredValue = "";
-
-            txtDisplay.Text = Calculator.EnteredValue;
-            lblDisplay.Content = Calculator.StoredValue;
-            lblOperator.Content = Calculator.Operator;
         }
 
+        private void Evaluate(string mathOperator)
+        {
+            try
+            {
+                if (Calculator.StoredValue == "")
+                {
+                    Calculator.StoredValue = Calculator.EnteredValue;
+                }
+                else if (Calculator.StoredValue != "" && Calculator.EnteredValue != "")
+                {
+                    var expression = Calculator.StoredValue + Calculator.Operator + Calculator.EnteredValue;
+                    double answer;
+                    answer = Convert.ToDouble(new DataTable().Compute(expression, null));
+                    Calculator.StoredValue = answer.ToString();
+                }
 
+                Calculator.EnteredValue = "";
+
+                txtDisplay.Text = Calculator.EnteredValue;
+                lblDisplay.Content = Calculator.StoredValue;
+                lblOperator.Content = Calculator.Operator;
+            }
+            catch
+            {
+                lblDisplay.Content = "Invalid Expression";
+            }
+        }
+
+        private void BtnPositiveNegative_Click(object sender, RoutedEventArgs e)
+        {
+            if (Calculator.EnteredValue == "")
+            {
+                Calculator.EnteredValue = "-";
+            }
+            else if (Calculator.EnteredValue == "-")
+            {
+                Calculator.EnteredValue = "";
+            }
+            else
+            {
+                var val = Convert.ToDouble(Calculator.EnteredValue);
+                if (val > 0)
+                {
+                    Calculator.EnteredValue = Calculator.EnteredValue.Insert(0, "-");
+                }
+                else
+                {
+                    Calculator.EnteredValue = Calculator.EnteredValue.Remove(0, 1);
+                }
+            }
+            txtDisplay.Text = Calculator.EnteredValue;
+        }
     }
 }
